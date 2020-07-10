3dbd7cb4462fabc2dc02209c4020b54d4042e9b5
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-10 17:19:13 -0300

    Manipulando as Collections dentro do Customer e do Order

--- Store.Domain/StoreContext/Entities/Customer.cs
+++ Store.Domain/StoreContext/Entities/Customer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Store.Domain.StoreContext.ValueObjects;
 
@@ -6,20 +7,29 @@ namespace Store.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
         public Customer(Name name, Document document, Email email, string phone)
         {
             Name = name;
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        // public IReadOnlyCollection<Address> Addresses { get { return _addresses.ToArray(); } }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            // Validar o endereço
+            // Adicionar o endereço
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
--- Store.Domain/StoreContext/Entities/Order.cs
+++ Store.Domain/StoreContext/Entities/Order.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Store.Domain.StoreContext.Enums;
 
@@ -6,32 +7,39 @@ namespace Store.Domain.StoreContext.Entities
 {
     public class Order
     {
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
         public Order(Customer customer)
         {
             Customer = customer;
             Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8);
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
-            Items = new List<OrderItem>();
-            Deliveries = new List<Delivery>();
+            _items = new List<OrderItem>();
+            _deliveries = new List<Delivery>();
         }
 
         public Customer Customer { get; private set; }
         public string Number { get; private set; }
         public DateTime CreateDate { get; private set; }
         public EOrderStatus Status { get; private set; }
-        public IReadOnlyCollection<OrderItem> Items { get; private set; }
-        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items { get { return _items.ToArray(); } }
+        public IReadOnlyCollection<Delivery> Deliveries { get { return _deliveries.ToArray(); } }
+
+        public void AddItem(OrderItem item)
+        {
+            _items.Add(item);
+        }
+
+        public void AddDelivery(Delivery delivery)
+        {
+            _deliveries.Add(delivery);
+        }
 
         // To Place An Order
         public void Place()
         {
 
-        }
-
-        public void AddItem(OrderItem item){
-            // Valida item
-            // Adiciona ao pedido
         }
     }
 }
