a389a7cd75dca77b3e5ae532d73179fd8a47fae3
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-12 16:53:20 -0300

    Criação do Command do Customer e implementação do Fast Fail Validation

---
+++ Store.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,29 @@
+using FluentValidator;
+using FluentValidator.Validation;
+
+namespace Store.Domain.StoreContext.CustomerCommands.Inputs
+{
+  public class CreateCustomerCommand : Notifiable
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+
+    // Fail Fast Validation -> Realiza as mesmas validações das entidades e VOs para evitar fazer requests desnecessários ao banco.
+    // Se por algum acaso esses testes abaixo falhem, a validação continua sendo feita dentro das entidades e VOs, logo não tem problema.
+    public bool Valid()
+    {
+      AddNotifications(new ValidationContract()
+        .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 caracteres.")
+        .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres.")
+        .HasMinLen(LastName, 3, "LastName", "O sobrenome deve conter pelo menos 3 caracteres.")
+        .HasMaxLen(LastName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres.")
+        .IsEmail(Email, "Address", "O E-mail está inválido.")
+        .HasLen(Document, 11, "Document", "CPF inválido")
+      );
+      return Valid();
+    }
+  }
+}
