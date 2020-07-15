7d391e5ffefa59faf8e038d466ae348d4b94a399
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-14 22:32:52 -0300

    Finalização do CustomerHandler

--- Store.Domain/StoreContext/Handlers/CustomerHandler.cs
+++ Store.Domain/StoreContext/Handlers/CustomerHandler.cs
@@ -5,6 +5,8 @@ using Store.Domain.StoreContext.Entities;
 using Store.Domain.StoreContext.ValueObjects;
 using Store.Domain.StoreContext.CustomerCommands.Inputs;
 using Store.Domain.StoreContext.CustomerCommands.Outputs;
+using Store.Domain.StoreContext.Repositories;
+using Store.Domain.StoreContext.Services;
 
 namespace Store.Domain.StoreContext.Handlers
 {
@@ -14,8 +16,24 @@ namespace Store.Domain.StoreContext.Handlers
           ICommandHandler<AddAddressCommand>
 
   {
+    private readonly ICustomerRepository _repository;
+    private readonly IEmailService _emailService;
+    public CustomerHandler(ICustomerRepository repository, IEmailService emailService)
+    {
+      _repository = repository;
+      _emailService = emailService;
+    }
+
     public ICommandResult Handle(CreateCustomerCommand command)
     {
+      // Validating CPF...
+      if (_repository.CheckDocument(command.Document))
+        AddNotification("Document", "Este CPF já está em uso");
+
+      // Validating email...
+      if (_repository.CheckEmail(command.Email))
+        AddNotification("Email", "Este E-mail já está em uso");
+
       // VOs
       var name = new Name(command.FirstName, command.LastName);
       var document = new Document(command.Document);
@@ -29,13 +47,20 @@ namespace Store.Domain.StoreContext.Handlers
       AddNotifications(document.Notifications);
       AddNotifications(email.Notifications);
       AddNotifications(customer.Notifications);
-      
-      return new CreateCustomerCommandResult(Guid.NewGuid(), name.ToString(), email.Address);
+
+      if (Invalid)
+        return null;
+
+      _repository.Save(customer);
+
+      _emailService.Send(email.Address, "test@gmail.com", "Bem vindo", "Seja bem vindo");
+
+      return new CreateCustomerCommandResult(customer.Id, name.ToString(), email.Address);
     }
 
     public ICommandResult Handle(AddAddressCommand command)
     {
-      
+      throw new System.NotImplementedException();
     }
   }
 }
