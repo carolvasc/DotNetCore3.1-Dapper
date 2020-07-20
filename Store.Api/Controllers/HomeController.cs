04efd9edddbce559093b5d518029113b0f893d5c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-19 21:00:22 -0300

    Configuração e criação da primeira Controller da Api

---
+++ Store.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Api.Controllers
+{
+  public class HomeController : Controller
+  {
+    public string Get()
+    {
+      return "";
+    }
+  }
+}
--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +12,7 @@ namespace Store.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +22,8 @@ namespace Store.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.UseRouting();
 
