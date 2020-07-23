46d302e7b75c33e21cd18b7cfb661e8bea176c7a
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-22 23:02:30 -0300

    Implementação das queries no CustomerController

--- Store.Api/Controllers/CustomerController.cs
+++ Store.Api/Controllers/CustomerController.cs
@@ -4,58 +4,38 @@ using Store.Domain.StoreContext.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Store.Domain.StoreContext.ValueObjects;
 using Store.Domain.StoreContext.CustomerCommands.Inputs;
+using Store.Domain.StoreContext.Queries;
+using Store.Domain.StoreContext.Repositories;
 
 namespace Store.Api.Controllers
 {
     public class CustomerController : Controller
     {
+				private readonly ICustomerRepository _repository;
+				public CustomerController(ICustomerRepository repository)
+				{
+						_repository = repository;
+				}
+
         [HttpGet]
         [Route("customers")]
-        public List<Customer> Get()
+        public IEnumerable<ListCustomerQueryResult> Get()
         {
-            var name = new Name("Fernanda", "Montenegro");
-            var document = new Document("46718115533");
-            var email = new Email("teste@gmail.com");
-            var customer = new Customer(name, document, email, "5511974857485");
-
-            var customers = new List<Customer>();
-            customers.Add(customer);
-
-            return customers;
+            return _repository.Get();
         }
 
         [HttpGet]
         [Route("customers/{id:int}")]
-        public Customer GetById(Guid id)
+        public GetCustomerQueryResult GetById(Guid id)
         {
-            var name = new Name("Fernanda", "Montenegro");
-            var document = new Document("46718115533");
-            var email = new Email("teste@gmail.com");
-            var customer = new Customer(name, document, email, "5511974857485");
-
-            return customer;
+            return _repository.GetById(id);
         }
 
         [HttpGet]
         [Route("customers/{id:int}/orders")]
-        public List<Order> GetOrders(Guid id)
+        public IEnumerable<ListCustomerOrdersQueryResult> GetOrders(Guid id)
         {
-            var name = new Name("César", "Figueiredo");
-            var document = new Document("66162305031");
-            var email = new Email("cezinha@gmail.com");
-            var customer = new Customer(name, document, email, "5511974857485");
-
-            var order = new Order(customer);
-            var mouse = new Product("Dark Core SE", "Mouse Gamer", "mouse.jpg", 590M, 10);
-            var monitor = new Product("LG 25' Ultrawide", "Mouse Gamer", "monitor.jpg", 900M, 3);
-
-            order.AddItem(monitor, 5);
-            order.AddItem(mouse, 5);
-
-            var orders = new List<Order>();
-            orders.Add(order);
-
-            return orders;
+            return _repository.GetOrders(id);
         }
 
         [HttpPost]
