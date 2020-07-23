9a46ab2c3f5a9b609b01dac94389547535a19b11
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-22 23:04:22 -0300

    Indentação do código

--- Store.Api/Controllers/CustomerController.cs
+++ Store.Api/Controllers/CustomerController.cs
@@ -9,64 +9,64 @@ using Store.Domain.StoreContext.Repositories;
 
 namespace Store.Api.Controllers
 {
-    public class CustomerController : Controller
+  public class CustomerController : Controller
+  {
+    private readonly ICustomerRepository _repository;
+    public CustomerController(ICustomerRepository repository)
     {
-				private readonly ICustomerRepository _repository;
-				public CustomerController(ICustomerRepository repository)
-				{
-						_repository = repository;
-				}
-
-        [HttpGet]
-        [Route("customers")]
-        public IEnumerable<ListCustomerQueryResult> Get()
-        {
-            return _repository.Get();
-        }
-
-        [HttpGet]
-        [Route("customers/{id:int}")]
-        public GetCustomerQueryResult GetById(Guid id)
-        {
-            return _repository.GetById(id);
-        }
-
-        [HttpGet]
-        [Route("customers/{id:int}/orders")]
-        public IEnumerable<ListCustomerOrdersQueryResult> GetOrders(Guid id)
-        {
-            return _repository.GetOrders(id);
-        }
-
-        [HttpPost]
-        [Route("customers/{id:int}")]
-        public Customer Post([FromBody] CreateCustomerCommand command)
-        {
-            var name = new Name(command.FirstName, command.LastName);
-            var document = new Document(command.Document);
-            var email = new Email(command.Email);
-            var customer = new Customer(name, document, email, command.Phone);
-
-            return customer;
-        }
-
-        [HttpPut]
-        [Route("customers/{id:int}")]
-        public Customer Put([FromBody] CreateCustomerCommand command)
-        {
-            var name = new Name(command.FirstName, command.LastName);
-            var document = new Document(command.Document);
-            var email = new Email(command.Email);
-            var customer = new Customer(name, document, email, command.Phone);
-
-            return customer;
-        }
-
-        [HttpDelete]
-        [Route("customers/{id:int}")]
-        public object Delete(Guid id)
-        {
-            return new { message = "Cliente removido com sucesso!" };
-        }
+      _repository = repository;
     }
+
+    [HttpGet]
+    [Route("customers")]
+    public IEnumerable<ListCustomerQueryResult> Get()
+    {
+      return _repository.Get();
+    }
+
+    [HttpGet]
+    [Route("customers/{id:int}")]
+    public GetCustomerQueryResult GetById(Guid id)
+    {
+      return _repository.GetById(id);
+    }
+
+    [HttpGet]
+    [Route("customers/{id:int}/orders")]
+    public IEnumerable<ListCustomerOrdersQueryResult> GetOrders(Guid id)
+    {
+      return _repository.GetOrders(id);
+    }
+
+    [HttpPost]
+    [Route("customers/{id:int}")]
+    public Customer Post([FromBody] CreateCustomerCommand command)
+    {
+      var name = new Name(command.FirstName, command.LastName);
+      var document = new Document(command.Document);
+      var email = new Email(command.Email);
+      var customer = new Customer(name, document, email, command.Phone);
+
+      return customer;
+    }
+
+    [HttpPut]
+    [Route("customers/{id:int}")]
+    public Customer Put([FromBody] CreateCustomerCommand command)
+    {
+      var name = new Name(command.FirstName, command.LastName);
+      var document = new Document(command.Document);
+      var email = new Email(command.Email);
+      var customer = new Customer(name, document, email, command.Phone);
+
+      return customer;
+    }
+
+    [HttpDelete]
+    [Route("customers/{id:int}")]
+    public object Delete(Guid id)
+    {
+      return new { message = "Cliente removido com sucesso!" };
+    }
+  }
 }
--- Store.Api/Controllers/HomeController.cs
+++ Store.Api/Controllers/HomeController.cs
@@ -2,34 +2,34 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Store.Api.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    [HttpGet]
+    [Route("rota/01")]
+    public object Get()
     {
-        [HttpGet]
-        [Route("rota/01")]
-        public object Get()
-        {
-            return new { version = "Version 0.0.1" };
-        }
-
-        [HttpPost]
-        [Route("rota/01")]
-        public object Post()
-        {
-            return new { version = "Version 0.0.1" };
-        }
-
-        [HttpPut]
-        [Route("rota/01")]
-        public object Put()
-        {
-            return new { version = "Version 0.0.1" };
-        }
-
-        [HttpDelete]
-        [Route("rota/01")]
-        public object Delete()
-        {
-            return new { version = "Version 0.0.1" };
-        }
+      return new { version = "Version 0.0.1" };
     }
+
+    [HttpPost]
+    [Route("rota/01")]
+    public object Post()
+    {
+      return new { version = "Version 0.0.1" };
+    }
+
+    [HttpPut]
+    [Route("rota/01")]
+    public object Put()
+    {
+      return new { version = "Version 0.0.1" };
+    }
+
+    [HttpDelete]
+    [Route("rota/01")]
+    public object Delete()
+    {
+      return new { version = "Version 0.0.1" };
+    }
+  }
 }
