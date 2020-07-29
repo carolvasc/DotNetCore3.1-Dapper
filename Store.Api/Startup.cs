362e6d4a4ed40824dc8c863b2abfc7197e754fb3
Carol Vasconcelos <cvsouza04@gmail.com>
2020-07-28 21:23:22 -0300

    Configuração do Swagger (documentação)

--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Store.Domain.StoreContext.Handlers;
 using Store.Domain.StoreContext.Repositories;
 using Store.Domain.StoreContext.Services;
@@ -11,44 +12,57 @@ using Store.Infra.StoreContext.Repositories;
 
 namespace Store.Api
 {
-    public class Startup
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+      services.AddMvc();
+      services.AddMvc(option => option.EnableEndpointRouting = false);
+
+      services.AddResponseCompression();
+
+      services.AddScoped<DataContext, DataContext>();
+      services.AddTransient<ICustomerRepository, CustomerRepository>();
+      services.AddTransient<IEmailService, EmailService>();
+      services.AddTransient<CustomerHandler, CustomerHandler>();
+
+      services.AddSwaggerGen(x =>
+      {
         {
-            services.AddMvc();
-            services.AddMvc(option => option.EnableEndpointRouting = false);
-
-            services.AddResponseCompression();
-
-            services.AddScoped<DataContext, DataContext>();
-            services.AddTransient<ICustomerRepository, CustomerRepository>();
-            services.AddTransient<IEmailService, EmailService>();
-            services.AddTransient<CustomerHandler, CustomerHandler>();
+          x.SwaggerDoc("v1", new OpenApiInfo { Title = "Store", Version = "v1" });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-
-            app.UseResponseCompression();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
+      });
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseMvc();
+
+      app.UseResponseCompression();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store - v1");
+      });
+
+      app.UseRouting();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapGet("/", async context =>
+              {
+                await context.Response.WriteAsync("Hello World!");
+              });
+      });
+    }
+  }
 }
